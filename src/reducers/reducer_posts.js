9660f24c7653e41903fd8ce41d9c3bd5a7c0fa94
Ronak Tanna <ronak.rktanna@gmail.com>
2018-06-20 02:04:49 +0530

    Added ability to show a post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 import _ from 'lodash';
 
 // function getMutatedRepsonse(response) {
@@ -11,6 +11,15 @@ import _ from 'lodash';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case DELETE_POST:
+      return _.omit(state, action.payload);
+    case FETCH_POST:
+      // const post = action.payload.data;
+      // const newState = { ...state };
+      // newState[post.id] = psot;
+
+      // return newState;
+      return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default:
